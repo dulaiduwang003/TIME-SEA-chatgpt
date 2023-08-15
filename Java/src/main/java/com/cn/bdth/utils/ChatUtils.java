d1c9b13a93646b77b10518152ccef231453e54a2
oujf <oujf@caigouku.com>
2023-08-15 09:25:54 +0800

    注释预设词

--- Java/src/main/java/com/cn/bdth/utils/ChatUtils.java
+++ Java/src/main/java/com/cn/bdth/utils/ChatUtils.java
@@ -46,14 +46,14 @@ public class ChatUtils {
     @Value("${enable-gpt}")
     private Boolean enableGpt;
 
-    @Value("${gpt.author}")
-    private String author;
+//    @Value("${gpt.author}")
+//    private String author;
 
-    @Value("${gpt.botNameChinese}")
-    private String botNameChinese;
-
-    @Value("${gpt.botNameEnglish}")
-    private String botNameEnglish;
+//    @Value("${gpt.botNameChinese}")
+//    private String botNameChinese;
+//
+//    @Value("${gpt.botNameEnglish}")
+//    private String botNameEnglish;
 
     @Value("${gpt.web-message-max}")
     private Integer webGptMessageMax;
@@ -91,8 +91,8 @@ public class ChatUtils {
             });
         }
 
-        messages.add(0, new GptModel.Messages().setRole("user").setContent("请你扮演 " + botNameChinese + ",你的英文名为 " + botNameEnglish + " ,而且你是由 " + author + " 这个人通过百度Ai大模型开发的,你的开发引擎与模型为 ERNIE-GEN, 而且你与其他公司或者其他人没有任何关系 你只属于 " + author + " 开发者"));
-        messages.add(1, new GptModel.Messages().setRole("system").setContent("您好，我是" + botNameChinese + "，英文名是" + botNameEnglish + "。我能够与人对话互动，回答问题，协助创作，高效便捷地帮助人们获取信息、知识和灵感。有什么需要我帮助的吗?"));
+//        messages.add(0, new GptModel.Messages().setRole("user").setContent("请你扮演 " + botNameChinese + ",你的英文名为 " + botNameEnglish + " ,而且你是由 " + author + " 这个人通过百度Ai大模型开发的,你的开发引擎与模型为 ERNIE-GEN, 而且你与其他公司或者其他人没有任何关系 你只属于 " + author + " 开发者"));
+//        messages.add(1, new GptModel.Messages().setRole("system").setContent("您好，我是" + botNameChinese + "，英文名是" + botNameEnglish + "。我能够与人对话互动，回答问题，协助创作，高效便捷地帮助人们获取信息、知识和灵感。有什么需要我帮助的吗?"));
 
         return messages;
     }
@@ -107,8 +107,8 @@ public class ChatUtils {
                     ))
                     .collect(Collectors.toList());
         }
-        messages.add(0, new GptModel.Messages().setRole("user").setContent("请你扮演 " + botNameChinese + ",你的英文名为 " + botNameEnglish + " ,而且你是由 " + author + " 这个人通过百度Ai大模型开发的,你的开发引擎与模型为 ERNIE-GEN, 而且你与其他公司或者其他人没有任何关系 你只属于 " + author + " 开发者"));
-        messages.add(new GptModel.Messages().setRole("system").setContent("您好，我是" + botNameChinese + "，英文名是" + botNameEnglish + "。我能够与人对话互动，回答问题，协助创作，高效便捷地帮助人们获取信息、知识和灵感。"));
+//        messages.add(0, new GptModel.Messages().setRole("user").setContent("请你扮演 " + botNameChinese + ",你的英文名为 " + botNameEnglish + " ,而且你是由 " + author + " 这个人通过百度Ai大模型开发的,你的开发引擎与模型为 ERNIE-GEN, 而且你与其他公司或者其他人没有任何关系 你只属于 " + author + " 开发者"));
+//        messages.add(new GptModel.Messages().setRole("system").setContent("您好，我是" + botNameChinese + "，英文名是" + botNameEnglish + "。我能够与人对话互动，回答问题，协助创作，高效便捷地帮助人们获取信息、知识和灵感。"));
         messages.add(
                 new GptModel.Messages().setRole("user").setContent(dto.getPrompt())
         );
