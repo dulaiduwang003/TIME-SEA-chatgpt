4dfb39b376fe9a7605bd286393a13587bd5a6d1c
oujf <oujf@caigouku.com>
2023-08-15 12:50:19 +0800

    修正web

---
+++ Web/src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <NavigationBar/>
+  <!--    <RouterView v-slot="{ Component }">-->
+  <!--      &lt;!&ndash; TODO 要缓存 &ndash;&gt;-->
+  <!--      <KeepAlive>-->
+  <!--        <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive"></component>-->
+  <!--      </KeepAlive>-->
+  <!--      &lt;!&ndash; TODO 不缓存 &ndash;&gt;-->
+  <!--      <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive"></component>-->
+  <!--    </RouterView>-->
+  <LeftNavigationBar/>
+  <el-dialog
+      v-model="dialogVisible"
+      title="公告"
+      width="25%"
+      center
+      align-center
+  >
+    <span style="text-align: center">{{ context }}</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">
+          朕已阅
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+import {useStore} from "vuex";
+import LeftNavigationBar from "@/components/LeftNavigationBar.vue";
+import NavigationBar from "@/components/NavigationBar.vue";
+import {getAnnouncement} from "../api/BSideApi";
+import {onMounted, ref} from "vue";
+
+export default {
+  components: {LeftNavigationBar, NavigationBar},
+  setup() {
+    let store = useStore()
+    store.commit("initState");
+    const dialogVisible = ref(false)
+    const context = ref('')
+    onMounted(() => {
+      setTimeout(() => {
+        getAnnouncementData()
+      }, 100)
+    })
+
+    async function getAnnouncementData() {
+
+      try {
+        let announcement = await getAnnouncement();
+        if (announcement) {
+          let item = localStorage.getItem("announcement");
+          if (item !== null) {
+            let parse = JSON.parse(item);
+            if (parse.logotypeId !== announcement.logotypeId) {
+              localStorage.setItem("announcement", JSON.stringify(announcement))
+              context.value = announcement.context
+              dialogVisible.value = true
+            }
+          } else {
+            localStorage.setItem("announcement", JSON.stringify(announcement))
+            context.value = announcement.context
+            dialogVisible.value = true
+          }
+        }
+      } catch (e) {
+        console.log(e)
+      }
+
+
+    }
+
+    return {
+      dialogVisible,
+      getAnnouncementData,
+      context
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  display: flex;
+  font-family: 'PingFang SC', 'Helvetica Neue', Helvetica, 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+  box-sizing: border-box;
+  padding: 20px;
+  letter-spacing: 1px;
+}
+
+@media only screen and (max-width: 767px) {
+  #app {
+    padding: 0;
+  }
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  /*background: #f6f6f6;*/
+  background: white;
+  color: #303030;
+  width: 100%;
+  height: 100%;
+}
+
+/* TODO 滚动条样式*/
+::-webkit-scrollbar {
+  width: 4px;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0.2);
+}
+
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0);
+  border-radius: 0;
+  background: rgba(0, 0, 0, 0);
+}
+
+
+.login-dialog > header {
+  display: none;
+}
+
+.login-dialog > .el-dialog__body {
+  padding: 0 !important;
+}
+
+</style>
---
+++ Web/src/components/LoginDialog.vue
@@ -0,0 +1,223 @@
+<template>
+  <el-dialog v-model="dialogVisible" width="400px" class="login-dialog" :show-close="false" align-center @close="close"
+             style="border-radius: 10px;overflow-x: hidden">
+    <div class="login-box">
+      <div class="head">
+        <img :src="require('../assets/login-header.png')" alt="" style="height:80px"/>
+      </div>
+      <div style="text-align: center;font-size: 15px;font-weight: 550;padding-top: 30px">微信登录</div>
+      <div class="form">
+        <div class="content" v-if="qrCode">
+          <img :src="qrCode" style="width: 160px;height: 160px">
+          <div class="cover-div" v-if="isFailure">
+            二维码已失效
+          </div>
+        </div>
+      </div>
+      <div style="display: flex;justify-content: center;align-items: center;padding-top: 20px" v-if="isFailure">
+        <el-button type="primary" color="#626aef" @click="getLoginQRCode()">重新生成</el-button>
+      </div>
+      <div style="text-align: center;padding-top: 30px;font-size: 8px;padding-bottom: 50px">
+        打开微信扫一扫快速登录/注册
+      </div>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import {defineComponent, ref, watch} from "vue";
+
+import {GetUserInfo, GetWechatCode, isQrCodeLoginSucceed} from "../../api/BSideApi";
+import {ElNotification} from "element-plus";
+import store from "@/store";
+
+
+export default defineComponent({
+  name: "LoginDialog",
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, {emit}) {
+    let qrCode = ref('')
+    let loginLoading = ref(false)
+    let dialogVisible = ref(false)
+    let isLogin = ref(true)
+    // eslint-disable-next-line no-unused-vars
+    let verifyCode = ref('')
+    let isFailure = ref(false)
+    let timerId;
+    let lock = ref(false)
+    watch(() => props.show, (newValue) => {
+      if (newValue) {
+        getLoginQRCode()
+        dialogVisible.value = true
+
+      }
+    }, {
+      immediate: true
+    })
+
+
+    /**
+     * 获取登录二维码
+     */
+    async function getLoginQRCode() {
+      try {
+        let newVar = await GetWechatCode();
+        if (newVar) {
+          isFailure.value = false
+          verifyCode.value = newVar.verifyCode
+          qrCode.value = newVar.qrCode
+          timerId = setInterval(() => {
+            scanCodeLoginResults();
+          }, 5000);
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    /**
+     * 扫码登录结果
+     * @returns {Promise<void>}
+     */
+    async function scanCodeLoginResults() {
+      try {
+        let promise = await isQrCodeLoginSucceed(verifyCode.value);
+        if (promise) {
+          if (!lock.value) {
+            lock.value = true;
+            clearInterval(timerId);
+            localStorage.setItem('token', promise);
+            dialogVisible.value = false
+            loginLoading.value = false
+            try {
+              let res = await GetUserInfo()
+              store.commit("setUserinfo", res);
+              // eslint-disable-next-line no-empty
+            } catch (e) {
+              console.log(e)
+            }
+            ElNotification({
+              title: '登录成功',
+              message: '欢迎来到TIME-SEA-PLUS',
+              type: 'success',
+            })
+            location.reload();
+            clearInterval(timerId);
+            lock.value = false
+          }
+
+        }
+      } catch (e) {
+        clearInterval(timerId);
+        isFailure.value = true
+      }
+
+    }
+
+
+    function close() {
+      emit('close')
+    }
+
+    return {
+      qrCode,
+      loginLoading,
+      dialogVisible,
+      close,
+      isLogin,
+      getLoginQRCode,
+      isFailure
+    }
+  }
+});
+</script>
+
+<style scoped>
+
+.inputBox {
+  height: 40px;
+  width: 300px;
+}
+
+.login-box {
+
+  overflow: hidden;
+  width: 100%;
+  padding: 0;
+  background: #FFFFFF;
+}
+
+.cover-div {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: rgba(255, 255, 255, 0.8);
+  padding: 10px;
+  border-radius: 5px;
+  width: 150px;
+  height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+}
+
+.login-box > .head > img {
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+  user-select: none;
+}
+
+.form {
+  position: relative;
+}
+
+.submit-button {
+  width: 100%;
+  letter-spacing: 2px;
+  font-weight: 300;
+  margin-top: 15px;
+  --el-button-bg-color: #686efe;
+  border: none;
+}
+
+.submit-button:hover, .submit-button:focus, .submit-button:active {
+  background-color: #7d80ff;
+  outline: 0;
+}
+
+
+.form > .content {
+  padding-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.profile-avatar {
+  display: block;
+  position: absolute;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  border: 4px solid #ffffff;
+  top: -50px;
+  right: calc(50% - 50px);
+  z-index: 2;
+  user-select: none;
+}
+
+.switch {
+  text-align: center;
+  font-size: 14px;
+  cursor: pointer;
+}
+</style>
