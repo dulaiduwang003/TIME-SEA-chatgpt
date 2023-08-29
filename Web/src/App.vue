c42ff81fea90fb0e7220f4ea4b20ed806110c34e
xiaoye <1592922966@qq.com>
2023-08-29 23:46:52 +0800

    【缺陷修复】 1、修复1366*720屏幕尺寸下管理页面无法滚动，无法看到分页组件问题

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,41 +1,50 @@
 <template>
-  <NavigationBar/>
-  <LeftNavigationBar/>
-  <el-dialog class="announcement" v-model="dialogVisible" center align-center width="380px"
-             style="background-color: rgb(27,30,32)">
+  <NavigationBar />
+  <LeftNavigationBar />
+  <el-dialog
+    class="announcement"
+    v-model="dialogVisible"
+    center
+    align-center
+    width="380px"
+    style="background-color: rgb(27, 30, 32)"
+  >
     <span style="text-align: center">{{ context }}</span>
     <template #footer>
       <span class="dialog-footer">
-        <el-button type="primary" @click="dialogVisible = false" color="#626aef">
+        <el-button
+          type="primary"
+          @click="dialogVisible = false"
+          color="#626aef"
+        >
           朕已阅
         </el-button>
       </span>
     </template>
   </el-dialog>
-  <levitation-ball/>
+  <levitation-ball />
 </template>
 
 <script>
-import {useStore} from "vuex";
+import { useStore } from "vuex";
 import LeftNavigationBar from "@/components/LeftNavigationBar.vue";
 import LevitationBall from "@/components/LevitationBall.vue";
 import NavigationBar from "@/components/NavigationBar.vue";
-import {getAnnouncement} from "../api/BSideApi";
-import {onMounted, ref} from "vue";
+import { getAnnouncement } from "../api/BSideApi";
+import { onMounted, ref } from "vue";
 
 export default {
-  components: {LeftNavigationBar, NavigationBar,LevitationBall},
+  components: { LeftNavigationBar, NavigationBar, LevitationBall },
   setup() {
-    let store = useStore()
+    let store = useStore();
     store.commit("initState");
-    const dialogVisible = ref(false)
-    const context = ref('')
+    const dialogVisible = ref(false);
+    const context = ref("");
     onMounted(() => {
       setTimeout(() => {
-        getAnnouncementData()
-      }, 100)
-
-    })
+        getAnnouncementData();
+      }, 100);
+    });
 
     async function getAnnouncementData() {
       try {
@@ -45,31 +54,30 @@ export default {
           if (item !== null) {
             let parse = JSON.parse(item);
             if (parse.logotypeId !== announcement.logotypeId) {
-              localStorage.setItem("announcement", JSON.stringify(announcement))
-              context.value = announcement.context
-              dialogVisible.value = true
+              localStorage.setItem(
+                "announcement",
+                JSON.stringify(announcement)
+              );
+              context.value = announcement.context;
+              dialogVisible.value = true;
             }
           } else {
-            localStorage.setItem("announcement", JSON.stringify(announcement))
-            context.value = announcement.context
-            dialogVisible.value = true
+            localStorage.setItem("announcement", JSON.stringify(announcement));
+            context.value = announcement.context;
+            dialogVisible.value = true;
           }
         }
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     }
 
-
-
     return {
       dialogVisible,
-      context
-    }
-  }
-}
-
-
+      context,
+    };
+  },
+};
 </script>
 
 <style>
@@ -78,20 +86,19 @@ export default {
   border-radius: 10px;
 }
 
-
 @media (max-width: 767px) {
   .announcement {
     width: 70%;
   }
 }
 
-
 #app {
   width: 100%;
   height: 100%;
   flex-direction: column;
   display: flex;
-  font-family: 'PingFang SC', 'Helvetica Neue', Helvetica, 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+  font-family: "PingFang SC", "Helvetica Neue", Helvetica, "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   box-sizing: border-box;
   padding: 20px;
   letter-spacing: 1px;
@@ -108,11 +115,10 @@ body {
   margin: 0;
   padding: 0;
   /*background: #f6f6f6;*/
-  background: #1D2022FF;
+  background: #1d2022ff;
   color: #303030;
   width: 100%;
   height: 100%;
-
 }
 
 /* TODO 滚动条样式*/
@@ -132,7 +138,6 @@ body {
   background: rgba(0, 0, 0, 0);
 }
 
-
 .login-dialog > header {
   display: none;
 }
@@ -141,24 +146,23 @@ body {
   padding: 0 !important;
 }
 
-.el-switch__core{
+.el-switch__core {
   background: #393939 !important;
 }
-.el-input-group__append{
+.el-input-group__append {
   box-shadow: none !important;
 }
-.el-input-group__append{
+.el-input-group__append {
   background: none !important;
 }
 
 .el-pagination .btn-prev,
 .el-pagination .btn-next {
-  background-color: rgb(35,40,42) !important;
+  background-color: rgb(35, 40, 42) !important;
 }
 
 .el-pagination .el-pager li:not(.active) {
-  background-color: rgb(35,40,42) !important;
+  background-color: rgb(35, 40, 42) !important;
   color: darkgray !important;
 }
-
 </style>
