888c12c905189ed4189f9c42ad8dbf8a094cc7cc
xiaoye <1592922966@qq.com>
2023-09-09 21:13:57 +0800

    【确定基准颜色】

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <NavigationBar />
-  <LeftNavigationBar />
-  <el-dialog
-    class="announcement"
-    v-model="dialogVisible"
-    center
-    align-center
-    width="380px"
-    style="background-color: rgb(27, 30, 32)"
-  >
-    <span style="text-align: center">{{ context }}</span>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button
-          type="primary"
-          @click="dialogVisible = false"
-          color="#626aef"
-        >
-          朕已阅
-        </el-button>
-      </span>
-    </template>
-  </el-dialog>
-  <levitation-ball />
+  <div class="appWrapper">
+    <div class="appInner" :class="store.getters.themeInfo.className">
+      <NavigationBar />
+
+      <LeftNavigationBar />
+
+      <levitation-ball />
+
+      <el-dialog
+        class="announcement"
+        v-model="dialogVisible"
+        center
+        align-center
+        width="380px"
+        style="background-color: rgb(27, 30, 32)"
+      >
+        <span style="text-align: center">{{ context }}</span>
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button
+              type="primary"
+              @click="dialogVisible = false"
+              color="#626aef"
+            >
+              朕已阅
+            </el-button>
+          </span>
+        </template>
+      </el-dialog>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,6 +42,7 @@ import { onMounted, ref } from "vue";
 
 export default {
   components: { LeftNavigationBar, NavigationBar, LevitationBall },
+
   setup() {
     let store = useStore();
     store.commit("initState");
@@ -74,13 +82,30 @@ export default {
 
     return {
       dialogVisible,
+      store,
       context,
     };
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+.appWrapper {
+  width: 100%;
+  height: 100%;
+
+  * {
+    transition: all 0.2s cubic-bezier(0.65, 0.05, 0.36, 1);
+  }
+}
+
+.appInner {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
 .announcement {
   width: 25%;
   border-radius: 10px;
@@ -100,10 +125,9 @@ export default {
   font-family: "PingFang SC", "Helvetica Neue", Helvetica, "Hiragino Sans GB",
     "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   box-sizing: border-box;
-  padding: 20px;
+  // padding: 20px;
   letter-spacing: 1px;
 }
-
 @media only screen and (max-width: 767px) {
   #app {
     padding: 0;
@@ -114,9 +138,6 @@ html,
 body {
   margin: 0;
   padding: 0;
-  /*background: #f6f6f6;*/
-  background: #1d2022ff;
-  color: #303030;
   width: 100%;
   height: 100%;
 }
@@ -165,5 +186,61 @@ body {
 .el-pagination .el-pager li:not(.active) {
   background-color: rgb(35, 40, 42) !important;
   color: darkgray !important;
+}
+</style>
+
+<style lang="scss" scoped>
+// 夜间模式配色
+.darkMode {
+  --bgColor1: #222;
+  --bgColor2: #333;
+  --bgColor3: #444;
+  --themeColor: #686efe;
+  --themeTextColor: #eee;
+  --textColor1: #eee;
+  --textColor2: #ccc;
+  --textColor3: #999;
+  --textColor4: #666;
+  --bordercolor: #333;
+}
+// 日间模式配色
+.lightMode {
+  --bgColor1: #fff;
+  --bgColor2: #f6f6f6;
+  --bgColor3: #e6e6e6;
+  --themeColor: #686efe;
+  --themeTextColor: #eee;
+  --textColor1: #222;
+  --textColor2: #444;
+  --textColor3: #666;
+  --textColor4: #999;
+  --bordercolor: #eee;
+}
+
+// 样式选择器， 后续所有的模式 都可以写在这里，  数量多的时候再抽离
+:deep(.appInner) {
+  background: var(--bgColor1);
+  color: var(--textColor1);
+
+  /*
+  变量定义区域
+  需要定背景色五级、主题色五级、文本三级(正文、标题、提醒) 颜色
+backgroundColor
+  */
+
+  /*
+  方案：
+  1、app.vue 基于类名修改变量值；
+  2、子组件全部位置，都使用变量进行颜色复制；
+
+  为什么不在这个页面直接修改：
+  1、如果是单个文件直接修改样式，日后新建组件，还要找回来app.vue，有点累赘。
+
+  执行步骤：
+  1、【完成】检查css mode修改之后， 子组件的变量能否生效 能否生效
+  2、检查父子组件变量是否能够传递；
+  3、将所有颜色类名都找出来，替换为使用变量;
+  4、
+  */
 }
 </style>
--- Web/src/store/index.js
+++ Web/src/store/index.js
@@ -2,10 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        userinfo: false
+        userinfo: false,
+        themeSwitchList:[{
+            id:0,
+            name:"黑夜模式",
+            className:'darkMode',
+            switchText:'日间模式',
+        },{
+            id:1,
+            name:"日间模式",
+            className:'lightMode',
+            switchText:'黑夜模式',
+        }],
+        themeSwitchIndex:0,
     },
     getters: {
-        userinfo: state => state.userinfo
+        userinfo: state => state.userinfo,
+        themeInfo: state => state.themeSwitchList[state.themeSwitchIndex]
     },
     mutations: {
         logout(state) {
@@ -14,6 +27,10 @@ export default createStore({
         setUserinfo(state, info) {
             state.userinfo = info
             localStorage.setItem('user', JSON.stringify(info));
+        },
+        setThemeSwitchIndex(state, themeSwitchIndex) {
+            state.themeSwitchIndex = themeSwitchIndex
+            localStorage.setItem('themeSwitchIndex', JSON.stringify(state.themeSwitchList[state.themeSwitchIndex]));
         },
         initState(state) {
             let token = localStorage.getItem('token')
