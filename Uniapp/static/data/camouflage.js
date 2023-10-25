13e8f2745f62919b8049b4f23b7c1ecc564a95a9
bdth <87460202+dulaiduwang003@users.noreply.github.com>
2023-10-25 20:30:43 +0800

    Delete Uniapp directory

--- Uniapp/static/data/camouflage.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-export default {
-    content: "你好 盆友"
-}
--- Uniapp/utils/image.js
+++
@@ -1,6 +0,0 @@
-import env from "@/utils/env";
-
-
-export function conversionImage(uri) {
-    return env.imageBaseUrl + uri
-}
